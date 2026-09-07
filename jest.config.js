af19a893677963829c9dfd8a0cffd1bbc8843ea3
leovit <leonardogondor@gmail.com>
2026-09-07 20:19:24 -0300

    vvvv

--- jest.config.js
+++ jest.config.js
@@ -1,18 +1,13 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-    preset: "ts-jest",
-    testEnvironment: "jsdom",
-    transform: {
-        "^.+\\.(ts|tsx)$": "ts-jest",
-    },
-    globals: {
-        "ts-jest": {
-            tsconfig: "tsconfig.jest.json",
-        },
-    },
-    moduleNameMapper: {
-        "^@/(.*)$": "<rootDir>/$1",
-        "\\.(css|less|scss|sass)$": "identity-obj-proxy",
-    },
-    setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  transform: {
+    "^.+\\.(ts|tsx)$": ["ts-jest", { tsconfig: "tsconfig.jest.json" }],
+  },
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/$1",
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+  },
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
 };
