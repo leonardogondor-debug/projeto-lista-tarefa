1dbfd8f774644719d45c05a271e6ca24b4193c1a
leovit <leonardogondor@gmail.com>
2026-09-07 21:14:25 -0300

    gggggg

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-// @ts-expect-error - Next.js CSS import typings may not be loaded in this setup
 import "./globals.css";
 
 export const metadata: Metadata = {
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
-import { getTarefas } from "../lib/tarefas";
 import NovaTarefa from "../components/NovaTarefa";
 
 export default async function Home() {
-  const tarefas = await getTarefas();
-
+  
   return (
     <main>
       <NovaTarefa />
