b89eba02d37ec11089bbd79759cbf9cf41fada79
leovit <leonardogondor@gmail.com>
2026-09-08 23:37:42 -0300

    hhhhhh

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       <body className="min-h-full flex flex-col justify-start">
         
         {children}
-        </body>
+        </body> 
     </html>
   );
 } 
