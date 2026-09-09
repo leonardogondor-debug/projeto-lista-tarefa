19eac82161acc9a839f08d83a4432e47174d62d9
leovit <leonardogondor@gmail.com>
2026-09-08 23:29:33 -0300

    dididididi

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-
+   
 export const metadata: Metadata = {
   title: "Lista de Tarefas",
   description: "projeto para testar ferramentas de teste jest e testing-library",
